1b44e8ffccf207e19b6170eee7deea65cea1b612
hwicode <khc3796@naver.com>
2022-01-02 15:45:24 +0900

    Print calandar whenever input a month
    월마다 일 수가 다르기 때문에 조건문을 통해 일 수를 조정함

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -8,13 +8,18 @@ public class HelloGit {
 		return MAX_DAYS[month - 1];
 	}
 
-	public void printSampleCalandar() {
+	public void printSampleCalandar(int maxDay) {
 		System.out.println("일  월  화  수 목  금 토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		if (maxDay == 30) {
+			System.out.println("29 30");
+		} else if (maxDay == 31) {
+			System.out.println("29 30 31");
+		}
 	}
 
 	public static void main(String[] args) {
@@ -35,7 +40,9 @@ public class HelloGit {
 			} else if (month > 12 || month < 1) {
 				System.out.println("알맞은 값을 입력해주세요.");
 			} else {
-				System.out.printf("%d월은 %d일까지 있습니다.\n", month, calandar.getMaxDaysOfMonth(month));
+				int maxDay = calandar.getMaxDaysOfMonth(month);
+				calandar.printSampleCalandar(maxDay);
+				;
 			}
 		}
 
