aa21dd9c9236a9f56ec7d7f7501c8eb5b4f4164f
hwicode <khc3796@naver.com>
2022-01-02 22:18:58 +0900

    첫번째 요일을 입력받는 기능 추가 및 입력받은 요일부터 1일이 시작됨

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,6 +3,7 @@ public class HelloGit {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final String[] dayOfTheWeek = { "SU", "MO", "TU", "WE", "TH", "FR", "SA" };
 
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
@@ -21,15 +22,25 @@ public class HelloGit {
 		}
 	}
 
-	public void printCalandar(int year, int month) {
+	public void printCalandar(int year, int month, String firstDay) {
 		System.out.printf("    <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 
 		int maxDay = getMaxDaysOfMonth(year, month);
+		int emptyDay = 0;
+		for (int i = 0; i < dayOfTheWeek.length; i++) {
+			if (dayOfTheWeek[i].equals(firstDay)) {
+				emptyDay = i;
+			}
+		}
+
+		for (int i = 0; i < emptyDay; i++) {
+			System.out.print("   ");
+		}
 		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+			if ((i + emptyDay) % 7 == 0) {
 				System.out.println();
 			}
 		}
--- src/Prompt.java
+++ src/Prompt.java
@@ -13,6 +13,9 @@ public class Prompt {
 			System.out.println("월을 입력하세요.");
 			System.out.print("Month> ");
 			int month = scan.nextInt();
+			System.out.println("첫번째 요일을 입력하세요. (SU, MO, WE, TH, FR, SA)");
+			System.out.print("Day> ");
+			String firstDay = scan.next();
 
 			if (month == -1) {
 				System.out.println("Have a nice day!");
@@ -20,7 +23,7 @@ public class Prompt {
 			} else if (month > 12 || month < 1) {
 				System.out.println("알맞은 값을 입력해주세요.");
 			} else {
-				calandar.printCalandar(year, month);
+				calandar.printCalandar(year, month, firstDay);
 			}
 		}
 
